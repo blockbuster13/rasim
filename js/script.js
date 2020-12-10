fec9abe675ba7ba3d13fd4d806ae3824c11caafb
blockbuster13 <75580596+blockbuster13@users.noreply.github.com>
2020-12-10 20:14:56 +0400

    RASIM

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+	$('.carousele').slick(
+	{
+dots: true,
+  infinite: true,
+  speed: 500,
+  fade: true,
+		arrows: true,
+		autoplay: true,
+ autoplaySpeed: 2000,
+	});
+});
